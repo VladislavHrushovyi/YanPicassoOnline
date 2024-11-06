a2acb030d50b1b79447648a8a9bcb310542a7b49
vladg <vlad.grush.99@gmail.com>
2024-11-06 11:55:16 +0200

    re-create react app

---
+++ PicassoOnline-react/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button variant="secondary" className='border-2 bg-blue-300 px-4 py-2 '>Primary</Button>{' '}
+    </>
+  )
+}
+
+export default App
