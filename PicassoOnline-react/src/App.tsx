1ea24515703161717df01d5aa886b731ada9666e
vladg <vlad.grush.99@gmail.com>
2024-11-14 20:14:39 +0200

    trying connectiong to server

--- PicassoOnline-react/src/App.tsx
+++ PicassoOnline-react/src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from 'react-router-dom';
 import { appRouter } from './router/router';
+import { connector } from './connector/connector';
 
 function App() {
-
+  const drawConnector = connector;
   return (
     <>
       <Container fluid className='h-max'>
---
+++ PicassoOnline-react/src/connector/connector.ts
@@ -0,0 +1,8 @@
+import { HubConnectionBuilder } from "@microsoft/signalr";
+
+export const connector = new HubConnectionBuilder()
+.withUrl("http://localhost:5125/draw")
+.build();
+
+connector.start().then(() => console.log('Connected to SignalR hub'))
+.catch(err => console.error('Error connecting to hub:', err));
