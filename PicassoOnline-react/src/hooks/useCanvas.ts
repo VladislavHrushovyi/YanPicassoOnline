e7ec08f79fff4f7cc43dae634e1d173578be031e
vladg <vlad.grush.99@gmail.com>
2024-12-05 19:04:51 +0200

    big bug continue

--- PicassoOnline-react/src/hooks/useCanvas.ts
+++ PicassoOnline-react/src/hooks/useCanvas.ts
@@ -1,60 +1,61 @@
 import { RefObject, useState } from "react"
 
 export const useCanvas = () => {
-    const [canvasRef, setCanvasRef] = useState<RefObject<HTMLCanvasElement>>()
-    const [isDrawing, setIsDrawing] = useState<boolean>(false)
-    const setRef = (ref: RefObject<HTMLCanvasElement>) => {
-      setCanvasRef(_ => ref)
-    }
+  const [canvasRef, setCanvasRef] = useState<RefObject<HTMLCanvasElement>>()
+  const [isDrawing, setIsDrawing] = useState<boolean>(false)
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const setRef = (ref: RefObject<HTMLCanvasElement>) => {
+    setCanvasRef(_ => ref)
+  }
 
-    const getCoord = (e: React.MouseEvent<HTMLCanvasElement>) => {
-      const canvas = canvasRef?.current
-      if (canvas) {
-        console.log(e.clientX, "left")
-        console.log(e.clientY, "right")
+  const setPosition = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (e.buttons !== 1) {
+      return;
+    }
+    const canvasRect = canvasRef?.current?.getBoundingClientRect();
+    if (canvasRect) {
+      setPos(_ => {
         return {
-          x: e.clientX,
-          y: e.clientY
-        };
-      } else {
-        return { x: 0, y: 0 };
-      }
-    };
-
-    const draw = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>, lineWidth: number, hexColor: string) => {
-      setIsDrawing(true);
-      const context = canvasRef?.current?.getContext("2d")
-      if (context) {
-        const coord = getCoord(e)
-        console.log("DRAWINg")
-        context.beginPath();
-        context.lineWidth = lineWidth;
-        context.lineCap = "round";
-        context.strokeStyle = hexColor;
-        context.moveTo(coord.x, coord.y);
-      }
-    }
-
-    const start = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-      if (isDrawing) {
-        const context = canvasRef?.current?.getContext("2d")
-        if (context) {
-          const coord = getCoord(e)
-          context.lineTo(coord.x, coord.y);
-          context.stroke();
+          x: e.clientX - canvasRect.left,
+          y: e.clientY - canvasRect.top
         }
+      });
+    }
+  };
+
+  const draw = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>, lineWidth: number, hexColor: string) => {
+    setIsDrawing(true);
+    const context = canvasRef?.current?.getContext("2d")
+    if (context) {
+      setPosition(e)
+      context.beginPath();
+      context.lineWidth = lineWidth;
+      context.lineCap = "round";
+      context.strokeStyle = hexColor;
+      context.moveTo(pos.x, pos.y);
     }
   }
 
-    const stop = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-        setIsDrawing(false);
+  const start = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (isDrawing) {
+      const context = canvasRef?.current?.getContext("2d")
+      if (context) {
+        setPosition(e)
+        context.lineTo(pos.x, pos.y);
+        context.stroke();
+      }
     }
+  }
 
-    return {
-        canvasRef,
-        draw,
-        start,
-        stop,
-        setRef
-    }
+  const stop = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    setIsDrawing(false);
+  }
+
+  return {
+    canvasRef,
+    draw,
+    start,
+    stop,
+    setRef
+  }
 }
