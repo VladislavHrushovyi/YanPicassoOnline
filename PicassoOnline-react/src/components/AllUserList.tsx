59f884cf522958bc61bf69231a109226e51b9e37
vladg <vlad.grush.99@gmail.com>
2025-02-18 18:06:56 +0200

    some bug with showing active users for admin

--- PicassoOnline-react/src/components/AllUserList.tsx
+++ PicassoOnline-react/src/components/AllUserList.tsx
@@ -4,11 +4,12 @@ import { useAppSelector } from "../store/hooks"
 
 export const AllUserList = () => {
     const users = useAppSelector(state => state.appReducer.activeUsers)
+    console.log(users)
     return (
         <>
             <Stack>
                 {
-                    users?.map(i => i.name !== null ?
+                    users?.map(i => i.name ?
                         <UserListItem name={`${i.name} ${i?.connId.substring(0, 5)}`} isOwner={false} isAdministratible={true} />
                         : "")
                 }
